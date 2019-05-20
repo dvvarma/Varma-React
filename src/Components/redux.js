a8e0a8a39e95ce12d0f3941f1cfb86e5ace91587
dvvarma <dvvarma@gmail.com>
2019-05-20 13:55:07 -0500

    Edited Ones

---
+++ src/Components/redux.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+function mapStateToProps(state) {
+    return {
+
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+
+    };
+}
+
+class redux extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default connect(
+    mapStateToProps,
+)(redux);
